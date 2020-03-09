3cf33915107ef182f9eb3827d35627a7704fab74
Vu Quang Huy <42594372+huyhuyhuy1998@users.noreply.github.com>
2020-03-09 22:12:05 +0700

    change request param of get websites and get web owner

--- common/query-lib/webOwner/getWebOwner.js
+++ common/query-lib/webOwner/getWebOwner.js
@@ -1,11 +1,10 @@
 import { query, METHOD } from "../../query";
 import { API_URL, SERVICE } from "../../service";
 
-export const getWebOwner = ({ webOwnerId, email, token }) =>
+export const getWebOwner = ({ token }) =>
   query({
     method: METHOD.GET,
     url: `${API_URL}/${SERVICE.WEB_OWNER}/api/web-owner`,
-    params: { webOwnerId: webOwnerId },
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + token
--- common/query-lib/website/getAllWebsiteByWebOwner.js
+++ common/query-lib/website/getAllWebsiteByWebOwner.js
@@ -1,11 +1,10 @@
 import { query, METHOD } from "../../query";
 import { API_URL, SERVICE } from "../../service";
 
-export const getAllWebsiteByWebOwner = ({ webOwnerId, token }) =>
+export const getAllWebsiteByWebOwner = ({token }) =>
   query({
     method: METHOD.GET,
     url: `${API_URL}/${SERVICE.WEB_OWNER}/api/web-owner/websites`,
-    params: { webOwnerId: webOwnerId },
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + token
