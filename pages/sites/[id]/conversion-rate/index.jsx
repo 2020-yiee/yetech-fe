5b40ce047589d7d63af330a00c63ca3e83a9c5e6
Vu Quang Huy <42594372+huyhuyhuy1998@users.noreply.github.com>
2020-03-19 00:31:50 +0700

    add utc for from and to param

--- pages/sites/[id]/conversion-rate/index.jsx
+++ pages/sites/[id]/conversion-rate/index.jsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { Typography, Breadcrumb } from 'antd';
-
-import { SideBarDefault } from '../../../../component/sites/side-bar';
-import { FunnelList } from '../../../../component/sites/funnel-list';
-import { SkeletonPage } from '../../../../component/sites/skeleton-page';
-
-const Funnel = ({ id }) => (
-  <SkeletonPage id={id} sideBarActive={SideBarDefault.CONVERSION_RATE}>
-    <Breadcrumb>
-      <Breadcrumb.Item>Analytics</Breadcrumb.Item>
-      <Breadcrumb.Item>Funnel Tracking</Breadcrumb.Item>
-      <Breadcrumb.Item>All</Breadcrumb.Item>
-    </Breadcrumb>
-
-    <Typography.Title level={2}>Funnel Tracking</Typography.Title>
-    <FunnelList />
-  </SkeletonPage>
-);
-
-Funnel.getInitialProps = ({ query: { id } }) => {
-  return { id };
-};
-
-export default Funnel;
-=======
 import {
     Typography,
     Breadcrumb
@@ -60,4 +34,3 @@ import {
   
   export default Funnel;
   
->>>>>>> 3876d11b375b46595dcc65f16651b5d44db97719
