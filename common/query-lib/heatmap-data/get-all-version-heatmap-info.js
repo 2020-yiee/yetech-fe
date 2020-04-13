ee821fec4b9e4b2daec2dcba3a4f440bbc48e0a4
Vu Quang Huy <huy9816@gmail.com>
2020-04-13 13:27:04 +0700

    add get all version heatmap info query

---
+++ common/query-lib/heatmap-data/get-all-version-heatmap-info.js
@@ -0,0 +1,14 @@
+import { query, METHOD } from '../../query';
+import { API_URL, SERVICE } from '../../service';
+
+export const getAllVersionTrackingHeatmapInfo = (trackingHeatmapInfoID, token, cancelToken) =>
+  query({
+    method: METHOD.GET,
+    url: `${API_URL}/${SERVICE.TRACKING}/api/tracking-info/all-version`,
+    params: { trackingHeatmapInfoID },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
+    },
+    cancelToken,
+  });
